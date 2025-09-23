e22b4fc01d4eeba51144dbfa0e2b0e2a53e8ff53
SoaringFish <albertoaldaba@gmail.com>
2025-09-23 13:46:01 +0800

    complete exercise 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemToRemove) {
+    return array.filter(item => !itemToRemove.includes(item));
 };
 
 // Do not edit below this line
