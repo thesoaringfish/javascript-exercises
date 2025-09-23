cc651c1e3fe0241217d106a43d10b92c749a8a52
SoaringFish <albertoaldaba@gmail.com>
2025-09-23 13:22:37 +0800

    finish exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (str, times) {
 
+    let result = "";
+    if (times < 0) return 'ERROR';
+
+    for (let i = 0; i < times; i++) {
+        result += str;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
