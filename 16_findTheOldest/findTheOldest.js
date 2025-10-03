24ee88e8a26704ab8f5181d56c0db6fd59f6de1b
SoaringFish <albertoaldaba@gmail.com>
2025-10-03 15:41:03 +0800

    complete exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  return people.reduce((oldest, person) => {
+    const currentYear = new Date().getFullYear();
 
+    // If no yearOfDeath, use current year
+    const personAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+    const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+
+    return personAge > oldestAge ? person : oldest;
+  });
 };
 
 // Do not edit below this line
