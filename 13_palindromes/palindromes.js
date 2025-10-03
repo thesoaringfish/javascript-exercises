57dbab7fcb21a5873823841da0a266b1c772c924
SoaringFish <albertoaldaba@gmail.com>
2025-10-03 15:29:41 +0800

    finish exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  // Normalize string: lowercase and remove non-alphanumeric chars
+  let cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  
+  // Reverse the string
+  let reversed = cleanStr.split("").reverse().join("");
+  
+  // Compare original and reversed
+  return cleanStr === reversed;
 };
 
 // Do not edit below this line
