031947073b41be1534622d1618337a9786171653
SoaringFish <albertoaldaba@gmail.com>
2025-10-03 15:36:15 +0800

    finish exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  // Convert string to number if possible
+  n = Number(n);
 
+  // Reject invalid input (negative, NaN, decimals)
+  if (n < 0 || !Number.isInteger(n)) return "OOPS";
+
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  let a = 1, b = 1;
+  for (let i = 3; i <= n; i++) {
+    let next = a + b;
+    a = b;
+    b = next;
+  }
+  return b;
 };
 
 // Do not edit below this line
